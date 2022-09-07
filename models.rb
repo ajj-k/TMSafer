2945a36bb5f12989c70baecbb65571303f192137
EC2 Default User <ec2-user@ip-10-1-14-100.ap-northeast-1.compute.internal>
2022-09-07 11:14:22 +0000

    modify models.rb

--- models.rb
+++ models.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection
+ActiveRecord::Base.establish_connection(:production)
 
 class User < ActiveRecord::Base
     has_secure_password
